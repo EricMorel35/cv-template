3df44bafd34d44bbb76e9c2299d39204d0019428
EricMorel35 <eric.morel35@gmail.com>
2025-07-20 16:42:38 +0200

    Reorganization

--- app/contents/index.html.js
+++ app/contents/index.html.js
@@ -9,96 +9,81 @@ module.exports = {
     contact: {
         title: 'Contact',
         item1: {
+            caption: 'Adresse ',
+            data: '6 rue des forgerons<br>35410 Nouvoitou'
+        },
+        item2: {
             caption: 'Téléphone ',
             data: '06-07-45-72-43'
         },
-        item2: {
-            caption: 'E-Mail ',
-            data: '<a href="mailto:mona.guinel@yahoo.com?subject=Contact">mona.guinel@yahoo.com</a>'
-        },
         item3: {
-            caption: 'Adresse ',
-            data: `6 rue des forgerons<br>35410 Nouvoitou`
+            caption: 'E-Mail ',
+            data: `<a href="mailto:mona.guinel@yahoo.com?subject=Contact">mona.guinel@yahoo.com</a>`
         }
     },
     skills: {
         title: 'Compétences',
         item1:{
-            subject: 'Patience'
+            subject: 'Patience / Créativité'
         },
         item2:{
-            subject: 'Empathie'
+            subject: 'Empathie / Sens de l\'écoute'
         },
         item3:{
-            subject: 'Aptitudes aux premiers secours'
+            subject: 'Respect du secret professionnel'  
         },
         item4:{
-            subject: 'Sens de l\'écoute'
+            subject: 'Travail en équipe / Organisation'
         },
         item5:{
-            subject: 'Respect du secret professionnel'
-        },
-        item6:{
-            subject: 'Travail en équipe'
-        },
-	item7:{
-            subject: 'Créativité'
-        },
-	item8:{
-            subject: 'Ponctualité'
-        },
-        item9:{
-            subject: 'Organisation'
-        }
-    },
-    lang: {
-        title: 'Langues',
-        data: {
-            lang1: {
-                caption: 'Français ',
-                level: 'langue maternelle'
-            }
+            subject: 'Ponctualité / Gestion du temps'
         }
     },
     aboutMe: {
         firstName: 'Mona',
         lastName: 'Guinel',
         position: 'Auxiliaire de puériculture',
-        summary: `Souhaitant débuter une reconversion professionnelle, je recherche une formation d'auxiliaire de puériculture.<br>Bienveillante et organisée, j'aimerais contribuer au bien-être des enfants dans un environnement stimulant et sécurisant.`
+        summary: `Autonome et enthousiaste, j’aime animer des activités éducatives et ludiques,
+assurer la sécurité des enfants et favoriser leur socialisation.
+Bienveillante et organisée, je recherche un poste d'Auxiliaire de puériculture,
+pour mettre en pratique mes connaissances et contribuer au bien-être des
+enfants dans un environnement stimulant et sécurisant.`
     },
     experience:{
         title : 'Expériences',
         item1: {
             period: {
-                start: "06/2012",
-                end: date,
+                start: "Depuis 2012"
             },
-            location: 'St Erblon',
+            location: 'St Erblon / 35',
             firm: 'La Poste / CDI',
-            position: 'Agent de La Poste',
-            summary: `- Exécution rapide et efficace des tâches assignées.<br>
-- Collaboration en équipe en communiquant, en partageant les idées et en mutualisant les efforts.<br>
-- Organisation du travail en priorisant les tâches dans le but de répondre aux attentes et de respecter les délais.<br>
-- Adaptation rapide à différents interlocuteurs et différentes situations.`
+            position: 'Factrice',
+            summary: `- Distribution du courrier et des colis aux adresses résidentielles et commerciales<br> 
+    - Collecte des lettres et colis à déposer<br> 
+    - Respect des délais de livraison et qualité́ du service client<br> 
+    - Proposition de services additionnels comme la vente de timbres, l'enregistrement de lettres recommandées et la gestion des réclamations<br> 
+    - Bonne relation avec les clients en fournissant des informations précises et en répondant à leurs questions<br> 
+    - Exécution rapide et efficace des tâches assignées<br>
+    - Collaboration en équipe en communiquant, en partageant les idées et en mutualisant les efforts`
         },
 	item2: {
             period: {
-                start: "03/2010",
-                end: "02/2011",
+                start: "2010",
+                end: "2011",
             },
-            location: 'Redon',
-            firm: 'Mairie de Redon / Contrat d\'Accompagnement dans l\'Emploi',
-            position: 'Agent de services en écoles primaires',
-            summary: `- Nettoyage courant des infrastructures : vidage des poubelles, aspiration des bureaux, lavage des sols et désinfection des sanitaires.<br>
-- Surveillance des élèves durant les heures de repas pour assurer un environnement paisible.`
+            location: 'Redon / 35',
+            firm: 'Mairie de Redon / Contrat d\'Accompagnement à l\'Emploi',
+            position: 'Agent de services en écoles primaires et Animatrice (ALSH)',
+            summary: `- Nettoyage des différentes infrastructures<br> 
+- Surveillance des élèves durant les repas<br>
+- Animation de groupes (activités manuelles, observation des étoiles, etc…)`
         },
 	item3: {
 	    period: {
-                start: "03/2007",
-                end: "04/2007",
+                start: "2007"
             },
             location: 'Douala / Cameroun',
-            firm: 'Orphelinat St Jean de Deido / Stage',
+            firm: 'Orphelinat St Jean de Deïdo / Stage d\'un mois',
             position: 'Animatrice pour enfants',
             summary: `- Organisation de jeux et activités en plein air pour favoriser l'épanouissement des enfants.<br>
 - Assistance aux enfants dans l'apprentissage des compétences de vie comme la propreté et l'habillage.<br>
@@ -107,33 +92,30 @@ module.exports = {
     },
     education: {
         title: 'Diplômes',
-	item1: {
+        item1: {
             period: {
-                start: 2010,
-                end: 2011,
+                end: 2011
             },
-            location: 'Nantes',
+            location: 'Nantes / 44',
             school: 'CEMEA',
-            degree: 'Brevet d\'Aptitude aux Fonctions d\'Animateur',
+            degree: 'Brevet d\'Aptitude aux Fonctions d\'Animateur (BAFA)',
             summary: ``
         },
         item2: {
             period: {
-                start: 2006,
                 end: 2008
             },
-            location: 'St Jacut les Pins',
-            school: 'Lycée d\'Enseignement Général et Technologique Privé',
-            degree: 'Baccalauréat professionnel Services en Milieu Rural',
+            location: 'St Jacut les Pins / 56',
+            school: 'Lycée d\'Enseignement Général et Technologique Privé (LEGTP)',
+            degree: 'Baccalauréat Professionnel Services en Milieu Rural',
             summary: 'Mention Bien'
         },
         item3: {
             period: {
-                start: 2004,
                 end: 2006
             },
-            location: 'St Jacut les Pins',
-            school: 'Lycée d\'Enseignement Général et Technologique Privé',
+            location: 'St Jacut les Pins / 56',
+            school: 'Lycée d\'Enseignement Général et Technologique Privé (LEGTP)',
             degree: 'Brevet d\'Aptitude Professionnelle Agricole Services aux Personnes',
             summary: ''
         } 
@@ -142,11 +124,11 @@ module.exports = {
         title: 'Centres d\'intérêts',
         item1: {
             icon: 'icon--crea',
-            caption: 'Loisirs créatifs'
+            caption: 'Loisirs créatifs<br> (bijoux, quilling, dessins,...)'
         },
 	item2: {
             icon: 'icon--cuisine',
-            caption: 'Cuisine'
+            caption: 'Cuisine (pâtisserie)'
         },
         item3: {
             icon: 'icon--open-book',
